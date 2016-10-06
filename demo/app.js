6ea707eedf445b6928c1dd042d28de24e28a9a99
Tom Noogen <tom@tnguyen-imac.local>
2016-10-06 10:19:18 -0500

    remove others

--- demo/app.js
+++ demo/app.js
@@ -59,7 +59,8 @@ function toggleBaseLayers(el, layer1, layer2){
 function getOverlayLayers(el, switchId){
     $('.loader').show();
 
-    switchMap = {"countyonoffswitch": "cty2010", "cityonoffswitch":"mcd2015", "cononoffswitch":"cng2012", "ssonoffswitch":"sen2012_vtd2015", "shonoffswitch":"hse2012_vtd2015"}
+    //  switchMap = {"countyonoffswitch": "cty2010", "cityonoffswitch":"mcd2015", "cononoffswitch":"cng2012", "ssonoffswitch":"sen2012_vtd2015", "shonoffswitch":"hse2012_vtd2015"}
+    switchMap = {"ssonoffswitch":"sen2012_vtd2015", "shonoffswitch":"hse2012_vtd2015"}
     // console.log(typeof switchMap[switchId]);
    
     if(el.is(':checked')){
