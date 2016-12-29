7b0c54b6019926ef2357862fa50554bc0364e210
Tom Noogen <tom@tnguyen-imac.local>
2016-12-29 15:03:40 -0600

    fix url builder

--- url-builder/main.js
+++ url-builder/main.js
@@ -1,21 +1,14 @@
 var myApp = angular.module('myApp', []);
-
 app.filter('nospace', function () {
     return function (value) {
         var str = value.replace(/\s+/g, '');
         return str;
     };
 });
-
-myApp.factory('Data', function(){
-
-
-
+app.filter('escape', function () {
+    return window.encodeURIComponent;
 });
-
-app.run(function($rootScope,$window){
-
-   $rootScope.prompt = $window.prompt;
+myApp.factory('Data', function () {});
+app.run(function ($rootScope, $window) {
+    $rootScope.prompt = $window.prompt;
 });
-
-
